bd785898b6996af8c0dd287862e8e49d05c7e81b
Pedro Luiz <pedroluizlsg@gmail.com>
2022-07-25 14:46:29 -0300

    Adiciona requisito 1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,37 +4,93 @@ class Form extends Component {
   render() {
     return (
       <form>
-        <input type="text" data-testid="name-input" />
+        <label htmlFor="name-input">
+          Nome
+          <input
+            type="text"
+            id="name-input"
+            data-testid="name-input"
+            placeholder="Nome da carta"
+          />
+        </label>
         <br />
 
-        <input type="textarea" data-testid="description-input" />
+        <label htmlFor="description-input">
+          Descrição
+          <input
+            type="textarea"
+            id="description-input"
+            data-testid="description-input"
+            placeholder="Descrição da carta"
+          />
+        </label>
         <br />
 
-        <input type="number" data-testid="attr1-input" />
+        <label htmlFor="attr1-input">
+          Attr01
+          <input
+            type="number"
+            id="attr1-input"
+            data-testid="attr1-input"
+            placeholder="Primeiro atributo"
+          />
+        </label>
         <br />
 
-        <input type="number" data-testid="attr2-input" />
+        <label htmlFor="attr2-input">
+          Attr02
+          <input
+            type="number"
+            id="attr2-input"
+            data-testid="attr2-input"
+            placeholder="Segundo atributo"
+          />
+        </label>
         <br />
 
-        <input type="number" data-testid="attr3-input" />
+        <label htmlFor="attr3-input">
+          Attr03
+          <input
+            type="number"
+            id="attr3-input"
+            data-testid="attr3-input"
+            placeholder="Terceiro atributo"
+          />
+        </label>
         <br />
 
-        <input type="text" data-testid="image-input" />
+        <label htmlFor="image-input">
+          Imagem
+          <input
+            type="text"
+            id="image-input"
+            data-testid="image-input"
+            placeholder="URL da imagem"
+          />
+        </label>
         <br />
 
-        <select
-          name="rate-input"
-          id="rare-input"
-          data-testid="rare-input"
-        >
-          <option value="normal">Normal</option>
-          <option value="raro">Raro</option>
-          <option value="muitoraro">Muito raro</option>
-
-        </select>
+        <label htmlFor="rare-input">
+          Raridade da carta
+          <select
+            name="rate-input"
+            id="rare-input"
+            data-testid="rare-input"
+          >
+            <option value="normal">Normal</option>
+            <option value="raro">Raro</option>
+            <option value="muitoraro">Muito raro</option>
+          </select>
+        </label>
         <br />
 
-        <button type="submit" data-testid="save-button">Salvar</button>
+        <label htmlFor="trunfo-input">
+          Super Trunfo
+          <input type="checkbox" id="trunfo-input" data-testid="trunfo-input" />
+        </label>
+        <br />
+
+        <button type="submit" id="save" data-testid="save-button">Salvar</button>
 
       </form>
     );
